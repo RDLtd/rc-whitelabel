2575293466b59a6bf10f1fb808701bbe27c4ac4d
jmbrnrd <jmbarnrd@gmail.com>
2020-10-14 14:43:04 +0100

    Init.

---
+++ projects/rc-whitelabel-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rdl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rc-whitelabel-app';
+}
