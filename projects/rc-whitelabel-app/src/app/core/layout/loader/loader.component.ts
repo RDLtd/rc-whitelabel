cf95584081bcb898c89ee74ecd8cab5d89b08717
jmbrnrd <jmbarnrd@gmail.com>
2020-11-03 08:30:01 +0000

    Search

---
+++ projects/rc-whitelabel-app/src/app/core/layout/loader/loader.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rd-loader',
+  templateUrl: './loader.component.html'
+})
+export class LoaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/rc-whitelabel-app/src/app/features/search/search.component.ts
+++ projects/rc-whitelabel-app/src/app/features/search/search.component.ts
@@ -10,66 +10,67 @@ export class SearchComponent implements OnInit {
   @ViewChild('rdSearchInput') rdSearchInput!: ElementRef;
 
   searchString = '';
+  isLoaded = false;
+  apiAccessCode = 'FR0100';
+  apiKey = 'Hy56%D9h@*hhbqijsG$D19Bsshy$)kH2';
+  landmarks: any;
+  summary: any;
 
-  constructor(private channelService: ChannelService) { }
+  constructor(private api: ChannelService) {
+  }
 
   ngOnInit(): void {
+
+    this.getChannelInfo();
+    this.getRestaurantSummary();
+    this.getLandmarks();
+
     setTimeout( () => {
       this.rdSearchInput.nativeElement.focus();
-    }, 100);
+      this.isLoaded = true;
+    }, 500);
+
   }
 
   search(val: string): void {
     console.log('SEARCH', val);
   }
 
-  doTest(index: number): void {
-    // tslint:disable-next-line:variable-name
-    const channel_access_code = 'FR0100';
-    // tslint:disable-next-line:variable-name
-    const channel_access_api_key = 'Hy56%D9h@*hhbqijsG$D19Bsshy$)kH2';
-    switch (index) {
-      case 1: {
-        this.channelService.getChannelInfo(channel_access_code, channel_access_api_key).subscribe(
-          // tslint:disable-next-line:no-any
-          (data: any) => {
-            console.log(data);
-          },
-          // tslint:disable-next-line:no-any
-          (error: any) => {
-            console.log(error);
-          });
-        break;
-      }
-      case 2: {
-        this.channelService.getChannelLandmarks(channel_access_code, channel_access_api_key).subscribe(
-          // tslint:disable-next-line:no-any
-          (data: any) => {
-            console.log(data);
-          },
-          // tslint:disable-next-line:no-any
-          (error: any) => {
-            console.log(error);
-          });
-        break;
-      }
-      case 3: {
-        const params = 'test';
-        const lat = 43.695;
-        const lng = 7.266;
-        this.channelService.getRestaurants(channel_access_code, channel_access_api_key, params, lat, lng).subscribe(
-          // tslint:disable-next-line:no-any
-          (data: any) => {
-            console.log(data);
-          },
-          // tslint:disable-next-line:no-any
-          (error: any) => {
-            console.log(error);
-          });
-        break;
-      }
-    }
-
+  getChannelInfo(): void {
+    this.api.getChannelInfo(this.apiAccessCode, this.apiKey).subscribe(
+      (data: object) => {
+        console.log(data);
+      },
+      (error: object) => {
+        console.log(error);
+      });
+  }
+  getRestaurantSummary(): void{
+    const lat = 43.695;
+    const lng = 7.266;
+    this.api.getRestaurantsSummary(this.apiAccessCode, this.apiKey, lat, lng).subscribe(
+      (data: any) => {
+        console.log(data);
+        this.summary = data;
+        this.setCuisines();
+      },
+      (error: object) => {
+        console.log(error);
+      });
+  }
+  getLandmarks(): void {
+    this.api.getChannelLandmarks(this.apiAccessCode, this.apiKey).subscribe(
+      (data: any) => {
+        // console.log(data.landmarks);
+        this.landmarks = data.landmarks;
+        console.log(this.landmarks);
+      },
+      (error: object) => {
+        console.log(error);
+      });
   }
 
+  setCuisines(): void {
+    console.log(this.summary.cuisines);
+  }
 }
