536bd527703855953ebcb58685835ff66d899cff
jmbrnrd <jmbarnrd@gmail.com>
2020-11-16 15:42:38 +0000

    bugfix

--- projects/rc-whitelabel-app/src/app/app.component.ts
+++ projects/rc-whitelabel-app/src/app/app.component.ts
@@ -20,11 +20,13 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     // Grab Query parameters
     this.route.queryParams.subscribe((params: any) => {
-      this.config.channelAccessCode = params.code || 'EN0100';
-      this.config.channelAPIKey = params.key || 'Hy56%D9h@*hhbqijsG$D19Bsshy$)ss3';
+      this.config.channelAccessCode = params.code || 'FR0100';
+      this.config.channelAPIKey = params.key || 'Hy56eD9h@*hhbqijsG$D19Bsshy$)kH2';
       this.config.language = params.lang;
       this.config.testMode = params.testing;
+      console.log(params.code, params.key);
     });
+
     // Load config
     // Would like to move this to data service
     this.api.getChannelInfo(this.config.channelAccessCode, this.config.channelAPIKey)
