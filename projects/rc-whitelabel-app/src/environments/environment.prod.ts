747b43b1703d044527b87cd4a877059a5c773a30
James Barnard <jmbarnard@gmail.com>
2022-08-01 17:27:49 +0100

    Added G key

--- projects/rc-whitelabel-app/src/environments/environment.prod.ts
+++ projects/rc-whitelabel-app/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   testMode: false,
-  API_URL: 'https://rc-server-staging.herokuapp.com'
+  API_URL: 'https://rc-server-staging.herokuapp.com',
+  GOOGLE_MAP_API_KEY: 'AIzaSyBD2S6quV-RF8Ckz_18Xrxqw_blnzlLl_I'
 };
