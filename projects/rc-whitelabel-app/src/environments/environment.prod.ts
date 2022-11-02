68a51128cb3cbefb11430f06f200d0060972e4dd
James Barnard <jmbarnard@gmail.com>
2022-11-02 12:04:51 +0000

    Switch location bugfix

--- projects/rc-whitelabel-app/src/environments/environment.prod.ts
+++ projects/rc-whitelabel-app/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   testMode: false,
+  RDL_API: 'https://rdl-server-staging.herokuapp.com',
   API_URL: 'https://rc-server-staging.herokuapp.com',
-  // API_URL: 'http://localhost:4000',
   GOOGLE_MAP_API_KEY: 'AIzaSyBD2S6quV-RF8Ckz_18Xrxqw_blnzlLl_I',
   SPW_DOMAIN_PATH: 'https://spw.restaurantcollective.org.uk'
 };
