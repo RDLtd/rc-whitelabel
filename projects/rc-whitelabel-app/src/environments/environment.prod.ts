de3564c8bf52c649e4ee03b9d8f1bc61caf18de0
James Barnard <jmbarnard@gmail.com>
2024-01-16 11:18:45 +0000

    16 latest

--- projects/rc-whitelabel-app/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  testMode: false,
-  RDL_API: 'https://rdl-server-staging.herokuapp.com',
-  API_URL: 'https://rc-server-staging.herokuapp.com',
-  GOOGLE_MAP_API_KEY: 'AIzaSyBD2S6quV-RF8Ckz_18Xrxqw_blnzlLl_I',
-};
--- projects/rc-whitelabel-app/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  testMode: true,
-  RDL_API: 'http://localhost:5100',
-  API_URL: 'http://localhost:4000',
-  // RDL_API: 'https://rdl-server-staging.herokuapp.com',
-  // API_URL: 'https://rc-server-staging.herokuapp.com',
-  GOOGLE_MAP_API_KEY: 'AIzaSyBD2S6quV-RF8Ckz_18Xrxqw_blnzlLl_I',
-};
