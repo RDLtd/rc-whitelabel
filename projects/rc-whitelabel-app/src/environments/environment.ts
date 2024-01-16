06034f31d1051513b898e0e381e5b3c8e278e5ea
James Barnard <jmbarnard@gmail.com>
2024-01-16 11:53:12 +0000

    16 latest

---
+++ projects/rc-whitelabel-app/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  testMode: true,
+  RDL_API: 'http://localhost:5100',
+  API_URL: 'http://localhost:4000',
+  // RDL_API: 'https://rdl-server-staging.herokuapp.com',
+  // API_URL: 'https://rc-server-staging.herokuapp.com',
+  GOOGLE_MAP_API_KEY: 'AIzaSyAUOxHaeFFu1_c0sEpjRCNOAKnl6r8YMvY'
+
+};
